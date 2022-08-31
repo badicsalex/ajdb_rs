6861206b891634412bf6dac69caff1d5aa1e3c37
Alex Badics <admin@stickman.hu>
2022-08-31 17:41:21 +0200

    util: Implement date range

--- src/util.rs
+++ src/util.rs
@@ -4,11 +4,16 @@
 
 use chrono::NaiveDate;
 
-pub struct NaiveDateRange {}
+#[derive(Debug, Clone)]
+pub struct NaiveDateRange {
+    from: NaiveDate,
+    to: NaiveDate,
+}
 
+/// Half-open range of NaiveDate-s, meant to be an exact replacement for Range<NaiveDate>
 impl NaiveDateRange {
     pub fn new(from: NaiveDate, to: NaiveDate) -> Self {
-        todo!()
+        Self { from, to }
     }
 }
 
@@ -16,6 +21,37 @@ impl Iterator for NaiveDateRange {
     type Item = NaiveDate;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        if self.from >= self.to {
+            return None;
+        }
+        let result = self.from;
+        self.from = self.from.succ();
+        Some(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+
+    #[test]
+    fn test_date_range() {
+        let range = NaiveDateRange::new(
+            NaiveDate::from_ymd(2020, 2, 27),
+            NaiveDate::from_ymd(2020, 3, 3),
+        );
+        let dates_in_range: Vec<_> = range.collect();
+        assert_eq!(
+            dates_in_range,
+            vec![
+                NaiveDate::from_ymd(2020, 2, 27),
+                NaiveDate::from_ymd(2020, 2, 28),
+                NaiveDate::from_ymd(2020, 2, 29),
+                NaiveDate::from_ymd(2020, 3, 1),
+                NaiveDate::from_ymd(2020, 3, 2),
+            ]
+        )
     }
 }
