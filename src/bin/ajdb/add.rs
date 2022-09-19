3d0d7d9a4005f0ea147c4cf91c15450f103d37ab
Alex Badics <admin@stickman.hu>
2022-09-19 14:44:16 +0200

    cli: add some verbosity to the add command

--- src/bin/ajdb/add.rs
+++ src/bin/ajdb/add.rs
@@ -7,6 +7,7 @@ use std::path::PathBuf;
 use ajdb::{database::Database, persistence::Persistence, util::read_all};
 use anyhow::Result;
 use hun_law::structure::Act;
+use log::info;
 
 #[derive(Debug, clap::Args)]
 pub struct AddArgs {
@@ -16,6 +17,7 @@ pub struct AddArgs {
 pub fn cli_add_raw(args: AddArgs) -> Result<()> {
     let act: Act = hun_law::util::singleton_yaml::from_slice(&read_all(args.path)?)?;
     let date = act.publication_date;
+    info!("Adding {} to state at {}", act.identifier, date);
     let mut persistence = Persistence::new("db");
     let mut db = Database::new(&mut persistence);
     let mut state = db.get_state(date)?;
