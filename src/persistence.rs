c0772c9815e677e04ea4f95768c0c3264a66ba30
Alex Badics <admin@stickman.hu>
2022-10-17 18:20:58 +0200

    persistence: use atomic file writes

--- src/persistence.rs
+++ src/persistence.rs
@@ -2,10 +2,11 @@
 // Copyright 2022, Alex Badics
 // All rights reserved.
 
+use std::path::Path;
 use std::{fs, io::Write, path::PathBuf};
 
-use anyhow::Context;
 use anyhow::Result;
+use anyhow::{anyhow, Context};
 use flate2::write::GzDecoder;
 use flate2::write::GzEncoder;
 use flate2::Compression;
@@ -33,17 +34,14 @@ impl Persistence {
         }
     }
 
-    // NOTE: This function needs &mut self even though nothing is actually mutated.
-    //       this is to signify that the fs in fact is mutated and this operation
-    //       should probably be protected somehow.
-    //       Also if we did a fully in-memory storage, we would need &mut anyway.
+    /// Atomically store data at key. Reentrant, but order between concurrent saves is not guaranteed.
     pub fn store<T: serde::Serialize>(
-        &mut self,
+        &self,
         input_key: KeyType,
         data: &T,
     ) -> Result<PersistenceKey> {
         let the_json = serde_json::to_vec_pretty(data).with_context(|| {
-            anyhow::anyhow!(
+            anyhow!(
                 "Encoding to JSON failed for {:?}, value type={}",
                 input_key,
                 std::any::type_name::<T>()
@@ -68,17 +66,17 @@ impl Persistence {
         let mut gz_encoder = GzEncoder::new(Vec::new(), Compression::default());
         gz_encoder
             .write_all(&the_json)
-            .with_context(|| anyhow::anyhow!("Compression failed for {}", key))?;
+            .with_context(|| anyhow!("Compression failed for {}", key))?;
         let gz_encoded_data = gz_encoder
             .finish()
-            .with_context(|| anyhow::anyhow!("Compression finish failed for {}", key))?;
+            .with_context(|| anyhow!("Compression finish failed for {}", key))?;
 
         if let Some(file_dir) = file_path.parent() {
             fs::create_dir_all(file_dir)
-                .with_context(|| anyhow::anyhow!("Creating directories failed for {}", key))?;
+                .with_context(|| anyhow!("Creating directories failed for {}", key))?;
         }
-        fs::write(file_path, gz_encoded_data)
-            .with_context(|| anyhow::anyhow!("Writing file data failed for {}", key))?;
+        Self::atomic_write(&file_path, &gz_encoded_data)
+            .with_context(|| anyhow!("Writing file data failed for {}", key))?;
         Ok(key)
     }
 
@@ -111,5 +109,24 @@ impl Persistence {
             hash >> 56,
             hash & 0xFFFFFFFFFFFFFF
         )
+    }
+
+    fn atomic_write(path: &Path, bytes: &[u8]) -> Result<()> {
+        let file_dir = path
+            .parent()
+            .map(|p| p.to_path_buf())
+            .unwrap_or_else(|| PathBuf::from("."));
+        let name = path
+            .file_name()
+            .ok_or_else(|| anyhow!("No filename found in atomic_write: {:?}", path))?;
+        fs::create_dir_all(&file_dir)?;
+        let mut tmp_fil = tempfile::Builder::new()
+            .prefix(name)
+            .suffix(".tmp")
+            .tempfile_in(&file_dir)?;
+        tmp_fil.write_all(bytes)?;
+        tmp_fil.flush()?;
+        tmp_fil.persist(path)?;
+        Ok(())
     }
 }
