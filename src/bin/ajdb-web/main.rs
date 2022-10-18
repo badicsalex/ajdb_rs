26660cdfd7b834aa461e1314dc7f49e77d5e2d75
Alex Badics <admin@stickman.hu>
2022-10-18 17:56:04 +0200

    web: actually print logs

--- src/bin/ajdb-web/main.rs
+++ src/bin/ajdb-web/main.rs
@@ -2,9 +2,17 @@
 // Copyright 2022, Alex Badics
 // All rights reserved.
 
+use std::io::Write;
+
 use ajdb::web::web_main;
 
 #[tokio::main]
 async fn main() {
+    env_logger::Builder::from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    )
+    .format(|buf, record| writeln!(buf, "{:>5}: {}", record.level(), record.args()))
+    .init();
+
     web_main().await
 }
