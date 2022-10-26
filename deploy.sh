8873df1c149f659c853473f3c0d9eecf30a72764
Alex Badics <admin@stickman.hu>
2022-10-26 18:29:49 +0200

    deploy.sh: deploy database too

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 set -ex
 
 cargo build --release
+chmod -R a+r db
 rsync target/release/ajdb-web ajdb.hu:/opt/ajdb/ajdb-web-new
 rsync -a src/web/static/ ajdb.hu:/opt/ajdb/src/web/static/
+rsync -a db/ ajdb.hu:/opt/ajdb/db/
 ssh ajdb.hu -A "
     cd /opt/ajdb;
     sudo systemctl stop ajdb-web &&
