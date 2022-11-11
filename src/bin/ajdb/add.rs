2cc08f6e7ad23ec4d2e68a6860782d2add9614d1
Alex Badics <admin@stickman.hu>
2022-11-11 16:41:53 +0100

    cli: add to accept more than one file

--- src/bin/ajdb/add.rs
+++ src/bin/ajdb/add.rs
@@ -2,25 +2,41 @@
 // Copyright 2022, Alex Badics
 // All rights reserved.
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use ajdb::{database::ActSet, persistence::Persistence, util::read_all};
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use hun_law::structure::Act;
 use log::info;
 
 #[derive(Debug, clap::Args)]
 pub struct AddArgs {
-    path: PathBuf,
+    #[clap(required = true, name = "path")]
+    paths: Vec<PathBuf>,
 }
 
 pub fn cli_add_raw(args: AddArgs) -> Result<()> {
+    let mut everything_ok = true;
+    for path in &args.paths {
+        if let Err(err) = add_path(path) {
+            log::error!("{err:?}");
+            everything_ok = false;
+        }
+    }
+    if everything_ok {
+        Ok(())
+    } else {
+        Err(anyhow!("Some acts were not processed"))
+    }
+}
+
+fn add_path(path: &Path) -> Result<()> {
     let act: Act = hun_law::util::singleton_yaml::from_slice(
-        &read_all(&args.path).with_context(|| anyhow::anyhow!("Error reading {:?}", args.path))?,
+        &read_all(path).with_context(|| anyhow!("Error reading {path:?}"))?,
     )
-    .with_context(|| anyhow::anyhow!("Error deserializing {:?}", args.path))?;
+    .with_context(|| anyhow!("Error deserializing {path:?}"))?;
     let date = act.publication_date;
-    info!("Adding {} to state at {}", act.identifier, date);
+    info!("Adding {} to state at {date}", act.identifier);
     let persistence = Persistence::new("db");
     let mut state = ActSet::load(&persistence, date)?;
     state.store_act(act)?;
