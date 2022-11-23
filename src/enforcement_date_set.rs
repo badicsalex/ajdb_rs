f3851ee430288743bd8f11c63c39dfe488d740d7
Alex Badics <admin@stickman.hu>
2022-11-23 15:48:23 +0100

    enforcement_dates: support structural EDs.

--- src/enforcement_date_set.rs
+++ src/enforcement_date_set.rs
@@ -2,18 +2,21 @@
 // Copyright 2022, Alex Badics
 // All rights reserved.
 
-use anyhow::{ensure, Result};
+use anyhow::{anyhow, ensure, Result};
 use chrono::{Datelike, NaiveDate};
 use hun_law::{
-    identifier::IdentifierCommon,
-    reference::{parts::AnyReferencePart, Reference},
-    semantic_info::{EnforcementDate, SpecialPhrase},
-    structure::{Act, ChildrenCommon, SubArticleElement},
+    identifier::{
+        range::{IdentifierRange, IdentifierRangeFrom},
+        IdentifierCommon,
+    },
+    reference::{parts::AnyReferencePart, structural::StructuralReference, Reference},
+    semantic_info::{EnforcementDate, EnforcementDateType, SpecialPhrase},
+    structure::{Act, ActChild, ChildrenCommon, SubArticleElement},
     util::{debug::WithElemContext, walker::SAEVisitor},
 };
 use log::info;
 
-use crate::fixups::ActFixups;
+use crate::{fixups::ActFixups, structural_cut_points::GetCutPoints};
 
 #[derive(Debug)]
 pub struct ActualEnforcementDate {
@@ -41,35 +44,32 @@ impl EnforcementDateSet {
             );
             visitor.result.extend(additional_eds);
         }
-        Self::from_enforcement_dates(&visitor.result, act.publication_date)
+        Self::from_enforcement_dates(&visitor.result, act)
             .with_elem_context("Calculating enforcement dates failed", act)
     }
     pub fn from_enforcement_dates(
         raw_enforcement_dates: &[EnforcementDate],
-        publication_date: NaiveDate,
+        act: &Act,
     ) -> Result<Self> {
-        let default_dates: Vec<_> = raw_enforcement_dates
-            .iter()
-            .filter(|d| d.positions.is_empty())
-            .collect();
-        ensure!(
-            !default_dates.is_empty(),
-            "Could not find the default enforcement date (out of {})",
-            raw_enforcement_dates.len()
-        );
-        ensure!(
-            default_dates.len() == 1,
-            "Found too many default enforcement dates ({} out of {})",
-            default_dates.len(),
-            raw_enforcement_dates.len()
-        );
-        let default_date =
-            ActualEnforcementDate::from_enforcement_date(default_dates[0], publication_date)?.date;
-        let enforcement_dates = raw_enforcement_dates
-            .iter()
-            .filter(|d| !d.positions.is_empty())
-            .map(|d| ActualEnforcementDate::from_enforcement_date(d, publication_date))
-            .collect::<Result<Vec<_>>>()?;
+        let mut enforcement_dates = Vec::new();
+        let mut default_date = None;
+        for raw_date in raw_enforcement_dates {
+            let converted_date = ActualEnforcementDate::from_enforcement_date(raw_date, act)?;
+            if converted_date.positions.is_empty() {
+                ensure!(default_date.is_none(), "Found too many default enforcement dates (first: {default_date:?}, second: {raw_date:?})");
+                default_date = Some(converted_date);
+            } else {
+                enforcement_dates.push(converted_date);
+            }
+        }
+        let default_date = default_date
+            .ok_or_else(|| {
+                anyhow!(
+                    "Could not find the default enforcement date (out of {})",
+                    raw_enforcement_dates.len()
+                )
+            })?
+            .date;
 
         // See 61/2009. (XII. 14.) IRM rendelet 81. § (2)
         ensure!(
@@ -166,18 +166,28 @@ impl SAEVisitor for EnforcementDateAccumulator {
 }
 
 impl ActualEnforcementDate {
-    pub fn from_enforcement_date(
-        ed: &EnforcementDate,
-        publication_date: NaiveDate,
-    ) -> Result<Self> {
+    pub fn from_enforcement_date(ed: &EnforcementDate, act: &Act) -> Result<Self> {
         ensure!(
             ed.positions.iter().all(|p| p.act().is_none()),
             "Reference contained act in from_enforcement_date"
         );
-        let date = match ed.date {
-            hun_law::semantic_info::EnforcementDateType::Date(d) => d,
+        ensure!(
+            ed.structural_positions.iter().all(|p| p.act.is_none()),
+            "Structural reference contained act in from_enforcement_date"
+        );
+        let mut positions = ed.positions.clone();
+        for structural_ref in &ed.structural_positions {
+            positions.push(Self::convert_structural_ref(structural_ref, act)?)
+        }
+        let date = Self::convert_date(&ed.date, act.publication_date);
+        Ok(Self { positions, date })
+    }
+
+    fn convert_date(date: &EnforcementDateType, publication_date: NaiveDate) -> NaiveDate {
+        match date {
+            hun_law::semantic_info::EnforcementDateType::Date(d) => *d,
             hun_law::semantic_info::EnforcementDateType::DaysAfterPublication(num_days) => {
-                publication_date + chrono::Duration::days(num_days as i64)
+                publication_date + chrono::Duration::days(*num_days as i64)
             }
             hun_law::semantic_info::EnforcementDateType::DayInMonthAfterPublication {
                 month,
@@ -188,14 +198,34 @@ impl ActualEnforcementDate {
                 NaiveDate::from_ymd(
                     month_after_publication.year(),
                     month_after_publication.month(),
-                    day as u32,
+                    *day as u32,
                 )
             }
-        };
-        Ok(Self {
-            positions: ed.positions.clone(),
-            date,
-        })
+        }
+    }
+
+    fn convert_structural_ref(
+        structural_ref: &StructuralReference,
+        act: &Act,
+    ) -> Result<Reference> {
+        let cut = structural_ref.get_cut_points(act, false)?;
+        let mut articles = act.children[cut].iter().filter_map(|c| {
+            if let ActChild::Article(article) = c {
+                Some(article)
+            } else {
+                None
+            }
+        });
+        let first_id = articles
+            .next()
+            .ok_or_else(|| {
+                anyhow!(
+                    "Enforcement date structural ref did not contain articles ({structural_ref:?})"
+                )
+            })?
+            .identifier;
+        let last_id = articles.last().map(|a| a.identifier).unwrap_or(first_id);
+        Ok(IdentifierRange::from_range(first_id, last_id).into())
     }
 }
 
@@ -214,7 +244,7 @@ fn is_same_level(a: &AnyReferencePart, b: &AnyReferencePart) -> bool {
 
 #[cfg(test)]
 mod tests {
-    use hun_law::util::singleton_yaml;
+    use hun_law::{identifier::ActIdentifier, util::singleton_yaml};
     use pretty_assertions::assert_eq;
     use serde::{Deserialize, Serialize};
 
@@ -312,11 +342,200 @@ mod tests {
         let enforcement_dates: Vec<EnforcementDate> =
             singleton_yaml::from_str(TEST_ED_SET).unwrap();
         let test_refs: Vec<TestRef> = singleton_yaml::from_str(TEST_REFS).unwrap();
-        let ed_set = EnforcementDateSet::from_enforcement_dates(
-            &enforcement_dates,
-            NaiveDate::from_ymd(2013, 7, 1),
-        )
-        .unwrap();
+        let dummy_act = Act {
+            identifier: ActIdentifier {
+                year: 2024,
+                number: 420,
+            },
+            subject: "Testing".into(),
+            preamble: "".into(),
+            publication_date: NaiveDate::from_ymd(2013, 7, 1),
+            contained_abbreviations: Default::default(),
+            children: Vec::new(),
+        };
+        let ed_set =
+            EnforcementDateSet::from_enforcement_dates(&enforcement_dates, &dummy_act).unwrap();
+
+        for test_ref in test_refs {
+            let effective = TestRef {
+                position: test_ref.position.clone(),
+                date: ed_set.effective_enforcement_date(&test_ref.position),
+            };
+            assert_eq!(
+                singleton_yaml::to_string(&test_ref).unwrap(),
+                singleton_yaml::to_string(&effective).unwrap()
+            );
+        }
+    }
+
+    const TEST_ACT: &str = r#"
+        - StructuralElement:
+            identifier: "1"
+            title: BEVEZETŐ RENDELKEZÉSEK
+            element_type: Book
+        - Article:
+            identifier: "1:1"
+            children:
+              - body: Dummy article blah blah.
+        - Article:
+            identifier: "1:2"
+            children:
+              - body: Dummy article blah blah.
+        - Article:
+            identifier: "1:3"
+            children:
+              - body: Dummy article blah blah.
+        - Article:
+            identifier: "1:4"
+            children:
+              - body: Dummy article blah blah.
+        - StructuralElement:
+            identifier: "2"
+            title: AZ EMBER MINT JOGALANY
+            element_type: Book
+        - Article:
+            identifier: "2:1"
+            children:
+              - body: Dummy article blah blah.
+        - StructuralElement:
+            identifier: "3"
+            title: A JOGI SZEMÉLY
+            element_type: Book
+        - StructuralElement:
+            identifier: "1"
+            title: A JOGI SZEMÉLY ÁLTALÁNOS SZABÁLYAI
+            element_type:
+              Part: {}
+        - StructuralElement:
+            identifier: "1"
+            title: ÁLTALÁNOS RENDELKEZÉSEK
+            element_type: Title
+        - Article:
+            identifier: "3:1"
+            children:
+              - body: Dummy article blah blah.
+        - Article:
+            identifier: "3:2"
+            children:
+              - body: Dummy article blah blah.
+        - StructuralElement:
+            identifier: "2"
+            title: A JOGI SZEMÉLY LÉTESÍTÉSE
+            element_type: Title
+        - StructuralElement:
+            identifier: "1"
+            title: A létesítés szabadsága
+            element_type: Chapter
+        - Article:
+            identifier: "3:3"
+            children:
+              - body: Dummy article blah blah.
+        - StructuralElement:
+            identifier: "1a"
+            title: A létesítő okirat
+            element_type: Chapter
+        - Article:
+            identifier: "3:4"
+            children:
+              - body: Dummy article blah blah.
+        - StructuralElement:
+            identifier: "2"
+            title: A gazdasági társaság szervezete
+            element_type: Chapter
+        - Subtitle:
+            identifier: "1"
+            title: A gazdasági társaság legfőbb szerve
+        - Article:
+            identifier: "3:5"
+            children:
+              - body: Dummy article blah blah.
+        - Subtitle:
+            identifier: "1a"
+            title: Ügyvezetés és képviselet
+        - Article:
+            identifier: "3:6"
+            children:
+              - body: Dummy article blah blah.
+        - StructuralElement:
+            identifier: "3"
+            title: Másolat a gazdasági társaság szervezetéről
+            element_type: Chapter
+        - Subtitle:
+            identifier: "2"
+            title: Másolat a gazdasági társaság legfőbb szervéről
+        - Article:
+            identifier: "3:7"
+            children:
+              - body: Dummy article blah blah.
+        - Subtitle:
+            identifier: "3"
+            title: Másolat az ügyvezetés és képviseletről
+        - Article:
+            identifier: "3:8"
+            children:
+              - body: Dummy article blah blah.
+    "#;
+    const TEST_STRUCTURAL_ED_SET: &str = r#"
+        - date:
+            Date: 2013-07-15
+        - date:
+            Date: 2013-11-01
+          positions:
+            - article: "3:8"
+        - date:
+            Date: 2013-11-02
+          structural_positions:
+            - book: '3'
+              structural_element:
+                SubtitleRange:
+                  start: "1"
+                  end: "1a"
+        - date:
+            Date: 2013-11-03
+          structural_positions:
+            - book: '3'
+              structural_element:
+                Chapter: '1a'
+    "#;
+    const TEST_STRUCTURAL_REFS: &str = r#"
+        - position:
+            article: '3:3'
+          date: 2013-07-15
+        - position:
+            article: '3:4'
+          date: 2013-11-03
+        - position:
+            article: '3:5'
+          date: 2013-11-02
+        - position:
+            article: '3:6'
+          date: 2013-11-02
+        - position:
+            article: '3:7'
+          date: 2013-07-15
+        - position:
+            article: '3:8'
+          date: 2013-11-01
+    "#;
+
+    #[test]
+    fn test_structural_enforcement() {
+        let enforcement_dates: Vec<EnforcementDate> =
+            singleton_yaml::from_str(TEST_STRUCTURAL_ED_SET).unwrap();
+        let test_refs: Vec<TestRef> = singleton_yaml::from_str(TEST_STRUCTURAL_REFS).unwrap();
+        let dummy_act = Act {
+            identifier: ActIdentifier {
+                year: 2024,
+                number: 420,
+            },
+            subject: "Testing".into(),
+            preamble: "".into(),
+            publication_date: NaiveDate::from_ymd(2013, 7, 1),
+            contained_abbreviations: Default::default(),
+            children: singleton_yaml::from_str(TEST_ACT).unwrap(),
+        };
+        let ed_set =
+            EnforcementDateSet::from_enforcement_dates(&enforcement_dates, &dummy_act).unwrap();
 
         for test_ref in test_refs {
             let effective = TestRef {
